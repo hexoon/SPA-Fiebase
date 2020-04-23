8dd1b1c1e3755d90144457ad455a6e6bc026752f
hexoon <nano.bit00@gmail.com>
2020-04-23 07:07:32 -0500

    Update firebase_config.js

--- src/js/firebase_config.js
+++ src/js/firebase_config.js
@@ -1,13 +1,13 @@
 export const firebase_config = () => {
     var firebaseConfig = {
-        apiKey: "AIzaSyAPfCLxiFA43Ovy7_4rSk-4lw5p0x3oaCc",
-        authDomain: "intentologinface.firebaseapp.com",
-        databaseURL: "https://intentologinface.firebaseio.com",
-        projectId: "intentologinface",
-        storageBucket: "intentologinface.appspot.com",
-        messagingSenderId: "456284442373",
-        appId: "1:456284442373:web:a2269dfc53fafa12e1316c",
-        measurementId: "G-2ST3KTJE1G"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
